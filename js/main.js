7ff062e971eb904ef9ee49f6ac12b9091bbce48d
Junnel Crucillo <jcrucillo2@nait.ca>
2022-02-19 05:25:14 +0000

    Nivo Slider RC1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// $().load(function () {
+$(".slider-active").nivoSlider({
+    controlNav: true,
+    directionalNav: true,
+    randomStart: true,
+    controlNavThumbs: false,
+    animSpeed: 500,
+    pauseTime: 5000,
+    pauseOnHover: false,
+    manualAdvance: false,
+    prevText: '<i class="fa fa-angle-left"></i>',
+    nextText: '<i class="fa fa-angle-right"></i>',
+    slices: 15,
+    boxCols: 8,
+    boxRows: 4,
+});
+// });
